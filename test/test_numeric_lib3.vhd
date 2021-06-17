10855d5b7db5939625e2459afd2d9423386a530f
x74ea6f <70569142+x74ea6f@users.noreply.github.com>
2021-06-17 09:55:29 +0900

    update test

--- test/test_numeric_lib3.vhd
+++ test/test_numeric_lib3.vhd
@@ -1,3 +1,7 @@
+-- [TODO] Test for unsigned/signed vector.
+-- in Vivado, function can not return array(?).
+-- 
+
 library ieee;
 library std;
 library work;
@@ -29,9 +33,22 @@ architecture SIM of test_numeric_lib3 is
         print("");
     end procedure;
 
+
+    function get_uv(n: natural) return unsigned_v is
+        variable u_v: unsigned_v(0 to n-1);
+    begin
+        for i in u_v'range loop
+            u_v(i) := X"5";
+        end loop;
+        return u_v;
+    end function;
+
+
+
     --[TODO] Add to library.
-    type unsigned_vector is array (natural range <>, natural range <>) of std_ulogic;
-    type signed_vector is array (natural range <>, natural range <>) of std_ulogic;
+    type slv_vector is array (natural range <>, natural range <>) of std_logic;
+    type unsigned_vector is array (natural range <>, natural range <>) of std_logic;
+    type signed_vector is array (natural range <>, natural range <>) of std_logic;
 
     procedure print(u_v: unsigned_vector) is
         variable v: unsigned(u_v'range(2));
@@ -40,7 +57,7 @@ architecture SIM of test_numeric_lib3 is
             for j in u_v'range(2) loop
                 v(j) := u_v(i,j);
             end loop;
-            print(to_str(v) & ",", end_line=>False);
+            print(to_str(v) & ",", False);
         end loop;
         print("");
     end procedure;
@@ -54,6 +71,7 @@ architecture SIM of test_numeric_lib3 is
         return v;
     end function;
 
+    -- [TODO] can this systhesis?
     procedure set(u_v: inout unsigned_vector; n: natural; v: unsigned) is
     begin
         for j in u_v'range(2) loop
@@ -61,9 +79,10 @@ architecture SIM of test_numeric_lib3 is
         end loop;
     end procedure;
 
+
     --[TODO] not work in xsim.
-    function set_old(u_v: unsigned_vector; n: natural; v: unsigned) return unsigned_vector is
-        variable u_v2: unsigned_vector(u_v'range(1), u_v'range(2)):=(others=>(others=>'0'));
+    function set_func(u_v: unsigned_vector; n: natural; v: unsigned) return unsigned_vector is
+        variable u_v2: unsigned_vector(u_v'range(1), u_v'range(2)):=(others=>(others=>'1'));
     begin
         for i in u_v'range(1) loop
             for j in u_v'range(2) loop
@@ -77,6 +96,19 @@ architecture SIM of test_numeric_lib3 is
         return u_v2;
     end function;
 
+    --[TODO] not work in xsim.
+    function get_u_vec(n,m: natural) return unsigned_vector is
+        variable u_v: unsigned_vector(0 to n-1, m-1 downto 0):=(others=>(others=>'1'));
+    begin
+        for i in u_v'range(1) loop
+            for j in u_v'range(2) loop
+                u_v(i, j):= '1';
+            end loop;
+        end loop;
+        return u_v;
+    end function;
+    
+
 begin
     process is
         variable u_v: unsigned_v(0 to 2):=(
@@ -89,6 +121,7 @@ begin
        variable u_v3: unsigned_vector(0 to 2, 3 downto 0);
     begin
         print(u_v);
+        print(get_uv(3));
 
         for i in u_v2'range(1) loop
             for j in u_v2'range(2) loop
@@ -104,8 +137,10 @@ begin
         print(u_v2);
 
         print("TEST");
-        u_v3 := set_old(u_v2, 1, to_unsigned(1, 4));
+        u_v3 := set_func(u_v2, 1, to_unsigned(1, 4));
         print(u_v3);
+
+        print(get_u_vec(5,4));
 
         finish(0);
     end process;
